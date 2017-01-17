620b69a64cd8a177fca9e41a32d92b196e32ccff
Fabio Goncalves <fabio.goncalves@iMac-Fabio.local>
2017-01-17 14:21:13 +1100

    Add script to install apk and run it

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+adb -s Android.local shell pm uninstall com.example.androidthings.simplepio
+adb -s Android.local install ./blink/build/outputs/apk/blink-release.apk
+
+adb -s Android.local shell am start -n com.example.androidthings.simplepio/.BlinkActivity
+
