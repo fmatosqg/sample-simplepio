50a841d8910f2ec1f5d073c4b1cc30a2f4ff9360
Fabio de Matos <fmatos@monits.com>
2017-02-01 19:10:01 +1100

    Update config

--- run.sh
+++ run.sh
@@ -2,10 +2,9 @@
 
 ./gradlew :blink:assembleDebug
 adb -s Android.local shell pm uninstall com.example.androidthings.simplepio
-adb -s Android.local shell pm uninstall com.example.androidthings.simplepio.debug
 
 #adb -s Android.local install ./blink/build/outputs/apk/blink-release.apk
 adb -s Android.local install ./blink/build/outputs/apk/blink-debug.apk
 
 
-adb -s Android.local shell am start -n com.example.androidthings.simplepio.debug/.BlinkActivity
+adb -s Android.local shell am start -n com.example.androidthings.simplepio/.BlinkActivity
